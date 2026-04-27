7e97682369b3d6e3db427fec0425d62eadaf5512
Jonathan Durand <jdurand@udel.edu>
2026-04-27 13:59:24 -0400

    Launch File and deleted hello files

--- src/main/java/com/example/cis111bmoonphaseapp/FMXLMoonAppController.java
+++ src/main/java/com/example/cis111bmoonphaseapp/FMXLMoonAppController.java
@@ -1,12 +1,23 @@
 package com.example.cis111bmoonphaseapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import javafx.application.Platform;
 
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 public class FMXLMoonAppController implements Initializable {
---
+++ src/main/java/com/example/cis111bmoonphaseapp/MoonAppMain.java
@@ -0,0 +1,41 @@
+package com.example.cis111bmoonphaseapp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.util.*;
+
+public class MoonAppMain extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        // Load the GUI from FXML built in Scene Builder
+        Parent root = FXMLLoader.load(getClass().getResource("MoonFMXL.fxml"));
+        Scene scene = new Scene(root);
+        // The stylesheet is currently set in Scene Builder
+        // Note you can also load this programmatically
+        // scene.getStylesheets().add("styles.css");
+
+        stage.setScene(scene);
+        stage.show();
+
+        // All of the code that interacts with the API and the Scene
+        //   is found in FXMLTempAppController.java
+
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stop is called in Application class");
+    }
+
+}
