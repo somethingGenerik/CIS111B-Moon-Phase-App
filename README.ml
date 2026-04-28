ff69a53dd466611076bc1f307669f64c7a61b59f
somethingGenerik <lyshkong@gmail.com>
2026-04-28 18:49:56 -0400

    Update README.ml
    made the readme look more like Jasons

--- README.ml
+++ README.ml
@@ -1,5 +1,15 @@
-Moon Phase App
-Designed to grab current moon cycle and displays requested information
-single pannel GUI
-displays a photo matching the current moon phase
-information comes from moon-api.com
+JavaFX Moon-phase app
+Final project for CIS111B
+
+
+IMPORTANT FILES
+MoonAppMain.java
+MoonPhase.java
+FXMLMoonAppController.java
+Moon.java
+ApiInput.java
+MoonFXML
+
+Notes
+Built in IntelliJ
+API key is not stored in the repository, this is set by the IDE and accessed via the environment at run-time
