542d23e3ff6387dd7f75f2604acc70d74167007a
somethingGenerik <lyshkong@gmail.com>
2026-04-28 16:28:30 -0400

    Update README with Moon Phase App details
    adding description, will add more later.

--- README.ml
+++ README.ml
@@ -1,1 +1,5 @@
-Read me okay? thank you.
+Moon Phase App
+Designed to grab current moon cycle and displays requested information
+single pannel GUI
+displays a photo matching the current moon phase
+information comes from moon-api.com
